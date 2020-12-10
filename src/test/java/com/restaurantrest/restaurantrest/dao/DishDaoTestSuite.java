a6a226dadfaa5a9e726c2d9d36f1e8b51f41f713
marcinPluciennik <marcinpluciennik76@gmail.com>
2020-12-10 21:05:07 +0100

    Added one more test in DishDaoTestSuite

--- src/test/java/com/restaurantrest/restaurantrest/dao/DishDaoTestSuite.java
+++ src/test/java/com/restaurantrest/restaurantrest/dao/DishDaoTestSuite.java
@@ -58,4 +58,38 @@ public class DishDaoTestSuite {
         Assert.assertEquals(0, dish.getCartList().size());
     }
 
+    @Test
+    public void testSaveManyToMany(){
+        //Given
+        Dish dish1 = new Dish("Dish1", new BigDecimal("10"));
+        Dish dish2 = new Dish("Dish2", new BigDecimal("20"));
+        Dish dish3 = new Dish("Dish3", new BigDecimal("30"));
+        Cart cart1 = new Cart();
+        Cart cart2 = new Cart();
+
+        cart1.getDishList().add(dish1);
+        cart1.getDishList().add(dish1);
+        cart1.getDishList().add(dish2);
+        dish1.getCartList().add(cart1);
+        dish1.getCartList().add(cart1);
+        dish2.getCartList().add(cart1);
+
+        cart2.getDishList().add(dish3);
+        cart2.getDishList().add(dish2);
+        dish3.getCartList().add(cart2);
+        dish2.getCartList().add(cart2);
+
+        //When
+        dishDao.save(dish1);
+        long dish1Id = dish1.getDishId();
+        dishDao.save(dish2);
+        long dish2Id = dish2.getDishId();
+        dishDao.save(dish3);
+        long dish3Id = dish3.getDishId();
+
+        //Then
+        Assert.assertNotEquals(0, dish1Id);
+        Assert.assertNotEquals(0, dish2Id);
+        Assert.assertNotEquals(0, dish3Id);
+    }
 }
